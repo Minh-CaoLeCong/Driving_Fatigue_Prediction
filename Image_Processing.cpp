50b80b354000c4b34466ae106a05e27fae457525
caolecongminh <caolecongminh1997@gmail.com>
2020-10-07 17:47:26 +0700

    image pre-processing for haar cascades face detection

---
+++ Image_Processing.cpp
@@ -0,0 +1,11 @@
+#include "Image_Processing.h"
+
+using namespace cv;
+using namespace std;
+
+Mat ImageProcessing_Face_Detection_HaarCascade(Mat img)
+{
+	cvtColor(img, img, COLOR_RGB2GRAY); // convert RGB to GRAY
+
+	return img;
+}
---
+++ Image_Processing.h
@@ -0,0 +1,14 @@
+#ifndef IMAGE_PROCESSING_H
+#define IMAGE_PROCESSING_H
+
+#include "OpenCV440.h"
+
+using namespace cv;
+using namespace std;
+
+
+
+Mat ImageProcessing_Face_Detection_HaarCascade(Mat img);
+
+
+#endif // !IMAGE_PROCESSING_H
