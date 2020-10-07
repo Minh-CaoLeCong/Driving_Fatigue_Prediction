5eeffe78940183df90d0ca20778f3f7377efeafd
caolecongminh <caolecongminh1997@gmail.com>
2020-10-07 17:52:31 +0700

    init camera device

---
+++ Camera_Device.cpp
@@ -0,0 +1,62 @@
+#include "Camera_Device.h"
+
+using namespace cv;
+using namespace std;
+
+VideoCapture cap;
+Mat frame;
+
+// estimate FPS
+double FPS = 0.0;
+double FPS_time_elapsed = 0.0;
+int FPS_count_frame = 0;
+time_t FPS_Start_time, FPS_End_time;
+
+void Camera_Device_Ini()
+{
+	printf("[INFOR]: Initializing camera device.\n");
+
+	// Open camera
+	cap.open(CAMERA_DEVICE);
+
+	// Resolution
+	cap.set(CAP_PROP_FRAME_WIDTH, CAMERA_DEVICE_WIDTH);
+	cap.set(CAP_PROP_FRAME_HEIGHT, CAMERA_DEVICE_HEIGHT);
+
+	if (!cap.isOpened())
+		printf("[INFOR]: Could NOT open camera.\n");
+
+	return;
+}
+
+void Camera_Device_Test()
+{
+	Camera_Device_Ini();
+
+	while (1)
+	{
+		// estimate FPS
+		if (FPS_count_frame == 0)
+			time(&FPS_Start_time);
+
+		//cap >> frame;
+		cap.read(frame);
+		
+		// estimate FPS
+		FPS_count_frame++;
+		if (FPS_count_frame == FPS_NUM_FRAMES)
+		{
+			FPS_count_frame = 0;
+			time(&FPS_End_time);
+			FPS_time_elapsed = difftime(FPS_End_time, FPS_Start_time);
+			FPS = FPS_NUM_FRAMES / FPS_time_elapsed;
+		}
+		putText(frame, to_string(int(FPS)), Point(3, 13), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 2, 8, 0);
+
+		imshow("Test_Camera_Device", frame);
+
+		waitKey(1);
+		//if (waitKey(30) >= 0) break;
+	}
+	return;
+}
---
+++ Camera_Device.h
@@ -0,0 +1,40 @@
+#ifndef CAMERA_DEVICE_H
+#define CAMERA_DEVICE_H
+
+#include "OpenCV440.h"
+#include <time.h>
+
+// Init
+#define CAMERA_DEVICE 0
+#define CAMERA_DEVICE_WIDTH 320
+#define CAMERA_DEVICE_HEIGHT 240
+
+// estimate FPS
+#define FPS_NUM_FRAMES 60
+
+
+using namespace cv;
+
+extern VideoCapture cap;
+extern Mat frame;
+
+// estimate FPS
+extern double FPS;
+extern double FPS_time_elapsed;
+extern int FPS_count_frame;
+extern time_t FPS_Start_time;
+extern time_t FPS_End_time;
+
+extern void Camera_Device_Ini();
+
+extern void Camera_Device_Test();
+
+
+
+
+
+
+
+
+
+#endif // !CAMERA_DEVICE_H
