d459e6fc669cd95a5a6ae72efeb996cf03217da1
caolecongminh <caolecongminh1997@gmail.com>
2020-10-07 17:46:13 +0700

    OpenCV 4.4.0

---
+++ OpenCV440.h
@@ -0,0 +1,10 @@
+#ifndef OPENCV440_H
+#define OPENCV440_H
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
+
+#endif // !OPENCV440_H
